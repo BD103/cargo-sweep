c9ebe81d4b45afad35fac6217fc1392d241e25ab
BD103 <59022059+BD103@users.noreply.github.com>
2024-08-07 11:33:43 -0400

    feat: create initial action

---
+++ src/main.js
@@ -0,0 +1,38 @@
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+const io = require("@actions/io");
+
+async function buildCargoSweep() {
+    // Force install `cargo-sweep`, opting-in to SemVer-compatible updates from 0.7 upwards.
+    await exec.exec("cargo", ["install", "cargo-sweep", "--version", "^0.7.0", "--force"]);
+}
+
+function downloadCargoSweep() {
+    // TODO
+}
+
+/**
+ * @param {boolean} usePrebuilt 
+ */
+async function main(usePrebuilt) {
+    if (usePrebuilt) {
+        downloadCargoSweep();
+    } else {
+        await buildCargoSweep();
+    }
+
+    // Create timestamp.
+    await exec.exec("cargo", ["sweep", "--stamp"]);
+
+    // Move timestamp to `target/cargo-sweep`.
+    await io.mkdirP("target/cargo-sweep");
+    await io.mv("sweep.timestamp", "target/cargo-sweep");
+}
+
+try {
+    const usePrebuilt = core.getBooleanInput("use-prebuilt", { required: false });
+
+    await main(usePrebuilt);
+} catch (err) {
+    core.setFailed(`Action failed with error: ${err}`);
+}
---
+++ src/post.js
@@ -0,0 +1,17 @@
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+const io = require("@actions/io");
+
+async function main() {
+    // Move timestamp back to current directory.
+    await io.mv("target/cargo-sweep/sweep.timestamp", ".");
+
+    // Remove everything older than timestamp.
+    await exec.exec("cargo", ["sweep", "--file"]);
+}
+
+try {
+    await main();
+} catch (err) {
+    core.setFailed(`Action failed with error: ${err}`);
+}
