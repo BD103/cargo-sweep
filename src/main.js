1efb0933b3cf5ac002da881ae418c7208b02baf8
BD103 <59022059+BD103@users.noreply.github.com>
2024-08-20 21:43:30 -0400

    Revert "refactor: combine certain arguments together in `exec()` calls"
    This reverts commit ade6127ea2ae51101cfdde0fd94f3f274fe3817e.

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ const shared = require("./index");
 
 async function buildCargoSweep() {
     // Force install `cargo-sweep`, opting-in to SemVer-compatible updates from 0.7 upwards.
-    await exec.exec("cargo", ["install", "cargo-sweep", `--version ${shared.VERSION}`, "--no-track", "--force"]);
+    await exec.exec("cargo", ["install", "cargo-sweep", "--version", shared.VERSION, "--no-track", "--force"]);
 }
 
 async function downloadCargoSweep() {
@@ -58,7 +58,7 @@ async function downloadCargoSweep() {
     
     core.info("Verifying binary integrity using artifact attestations.");
     process.env["GH_TOKEN"] = ghToken;
-    await exec.exec("gh", ["attestation", "verify", shared.PATH, `--repo ${shared.REPO.owner}/${shared.REPO.repo}`]);
+    await exec.exec("gh", ["attestation", "verify", shared.PATH, "--repo", `${shared.REPO.owner}/${shared.REPO.repo}`]);
     delete process.env["GH_TOKEN"];
 }
 
