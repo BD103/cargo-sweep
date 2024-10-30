e171ad89ee355fd7599e6a0242c11084ea09f41a
BD103 <59022059+BD103@users.noreply.github.com>
2024-10-30 11:21:33 -0400

    fix: "Invalid Date" error

--- dist/main/index.js
+++ dist/main/index.js
@@ -26821,19 +26821,8 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(9093);
 
-/**
- * Returns the current timestamp.
- * 
- * This is equivalent to {@link Date#now}.
- * 
- * @returns {number} The current time since the UNIX epoch.
- */
-async function timestamp() {
-    return Date.now();
-}
-
 async function main() {
-    const stamp = timestamp();
+    const stamp = Date.now();
     core.info(`Creating timestamp at ${new Date(stamp)}. All files in the \`target\` folder not accessed between now and the end of the run will be purged.`);
     core.saveState("timestamp", stamp);
 }
