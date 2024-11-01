07d506c1e710df1e410b3d7808bfcaa6488fadb7
BD103 <59022059+BD103@users.noreply.github.com>
2024-11-01 13:02:40 -0400

    temp: test invalid javascript

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ async function main() {
     // Find the current time since the UNIX epoch.
     const stamp: number = Date.now();
 
+    Oh no!
+
     core.info(`Creating timestamp at ${new Date(stamp)}.`);
     core.info("All files in the `target` folder not accessed between now and the end of the run will be purged.")
 
